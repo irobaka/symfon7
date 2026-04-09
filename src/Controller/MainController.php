83cfa6c709c5bdec62393a0021dd6ee04629534c
Grzegorz Majta <grzegorz.majta@gmail.com>
2026-04-09 09:47:07 +0200

    wip

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -19,16 +20,21 @@ final class MainController extends AbstractController
         StarshipRepository $starshipRepository,
         HttpClientInterface $client,
         CacheInterface $issLocationPool,
+        #[Autowire(param: 'iss_location_cache_ttl')]
+        int $issLocationCacheTtl,
+        string $issLocationCacheKey,
     ): Response {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $issData = $issLocationPool->get('iss_location_data', function (ItemInterface $item) use ($client) {
-            $item->expiresAfter(20);
-            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+        $issData = $issLocationPool->get(
+            $issLocationCacheKey,
+            function (ItemInterface $item) use ($client, $issLocationCacheTtl) {
+                $item->expiresAfter($issLocationCacheTtl);
+                $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
 
-            return $response->toArray();
-        });
+                return $response->toArray();
+            });
 
         return $this->render('main/homepage.html.twig', [
             'ships' => $ships,
