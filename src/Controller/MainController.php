93f861eeb9e2d7966636cbfe6d2e2510038ee6e6
Grzegorz Majta <grzegorz.majta@gmail.com>
2026-04-09 08:20:33 +0200

    wip

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,18 +8,32 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function homepage(StarshipRepository $starshipRepository): Response
-    {
+    public function homepage(
+        StarshipRepository $starshipRepository,
+        HttpClientInterface $client,
+        CacheInterface $cache,
+    ): Response {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
+
+        $issData = $cache->get('iss_location_data', function (ItemInterface $item) use ($client) {
+            $item->expiresAfter(15);
+            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+
+            return $response->toArray();
+        });
 
         return $this->render('main/homepage.html.twig', [
             'ships' => $ships,
             'myShip' => $myShip,
+            'issData' => $issData,
         ]);
     }
 }
